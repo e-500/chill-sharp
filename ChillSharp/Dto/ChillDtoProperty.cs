9d19daac6cd9b20b3d2fa7c1a339cf60d21539ad
e-500 <andrea.piovesan@gmail.com>
2026-01-01 11:21:56 +0100

    Updated source files header comments related to LICENSE

--- ChillSharp/Dto/ChillDtoProperty.cs
+++ ChillSharp/Dto/ChillDtoProperty.cs
@@ -1,13 +1,20 @@
 ﻿/*
- * Author: Andrea Piovesan
- * Year: 2025
- * License: GNU Affero General Public License (AGPL) version 3
+ * ChillSharp is a lightweight .NET library that sits on top of Entity Framework Core 
+ * and turns an existing data model into a fully working REST API with almost no setup.
+ * Copyright (C) 2025 Andrea Piovesan
  *
- * Disclaimer:
- * You are free to use, modify, and distribute it under the terms of the AGPL v3 license.
- * This code comes with no warranty; use it at your own risk.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * For further information, please refer to README and LICENSE files.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 using System;
