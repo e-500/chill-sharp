b83a4330cba2c947c817fb444374beff38894887
e-500 <andrea.piovesan@gmail.com>
2026-02-08 18:05:58 +0100

    [Test] Added Author fields to test db context

--- ChillSharp.Tests/Migrations/20260202081035_v2.Designer.cs
+++ ChillSharp.Tests/Migrations/20260202081035_v2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChillSharp.Tests.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChillSharp.Tests.Migrations
 {
     [DbContext(typeof(DummyContext))]
-    partial class DummyContextModelSnapshot : ModelSnapshot
+    [Migration("20260202081035_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.22");
